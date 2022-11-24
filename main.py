e3515821b2d840873d48c7039ac83ceb7a8e482c
Ertai Blue <ertai.blue@gmail.com>
2022-11-24 17:19:58 +0100

    created print_blocks function

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def read_grid(path):
 		map = [[int(x) for x in line[:len(line)-1].split(' ')] for line in file.readlines()]
 		return map
 
-def print_grid(map, blocks):
-	#Maybe use escape characters to go back then print
+def print_grid(map):
+	#print the map
 	print('-' * (len(map[0]) + 2) )
-	for line in map: #maybe use enumerate or something. Like we put each line to display on the side in a list. Keep in mind that the map could have different sizes.
+	for line in map:
 		print('|', end= '')
 		for cell in line:
 			if cell == 0:
@@ -20,24 +20,47 @@ def print_grid(map, blocks):
 			elif cell == 1:
 				print('•', end='')
 			else:
-				print('⬛', end='')
+				print('▩', end='')
 		print('|')
-	print('-' * (len(map[0]) + 2) )
+	print('-' * (len(map[0]) + 2))
+	print(f"\033[{len(map)+2}A", f"\033[{len(map[0]) + 2}C", end='') #Move the cursor up len(map+2) lines, that it to the original position from which we start drawing the map.
+
+def print_blocks(blocks):
+	print(" " * 5, "Choose the next block to place", end=f"\033[{len('Choose the next block to place')}D\033[B")
+	#print the three randomly chosen blocks (should their scale be changed? cuz there is one block in the diamond map that looks like really big)
+
+	for block in blocks:
+		print('-' * (len(block[0])+2), end=f"\033[{len(block[0])+2}D\033[B")
+		
+		for line in block:
+			print('|', end='')
+			for cell in line:
+				if cell == 0:
+					print(' ', end='')
+				elif cell == 1:
+					print('•', end='')
+				else:
+					print('▩', end='')
+			print('|', end=f"\033[{len(line)+2}D\033[B")
+
+		print('-' * (len(block[0])+2), end=f"\033[{len(block[0])+2}D\033[B")		
+
+		print(f"\033[{len(block[0]) + 3}A\033[{len(block[0])+2}C", end='     ')
+
+	print(f"\033[80B") #put the cursor to the bottom of the terminal
+
 
 block_list = [ [[2, 0], [2, 0], [2, 2]], [[0, 2], [0, 2], [2, 2]]]
 map = read_grid("diamond.txt")
 game = True #boolean representing if the game is ongoing or not
 
 while game:
-	blocks = [0]
-	print_grid(map, blocks)
-	#also display the block
-	#we represent the block as a matrix, 2 being there is a full cell, and 0 being empty.
-	#to check if the coordinates are valid. Imagine we take the block and put it in the map matrix (we do this by putting the bottom left
-	#cell of the block at the given coordinates).
+	blocks = [block_list[0], block_list[1]]
+	print_grid(map)
+	print_blocks(blocks)
+
 	#The coordinates are invalid as long as for one cell, the sum of the block cell and the map cell is not equal to 3.
-	coordinates = tuple(int(x) for x in input("Enter the coordinates to place the block, separated by a comma   ").split(','))
-	#this is not the right way of doing this. I have to assert that the user input are of the right type and valid input.
+
 
 
 	game = False
